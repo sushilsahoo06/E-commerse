785bf0cd51d2b4a269dc6718f6a72bc7560b0b56
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-02 09:34:10 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const cookieParser=require('cookie-parser');
+const cors=require('cors');
 
 mongoose.connect(
   'mongodb+srv://sahoosushil456:lgPuiEoeUgO7pXIw@cluster0.mo007z3.mongodb.net/'
@@ -10,3 +12,23 @@ mongoose.connect(
 
 const app=express();
 const PORT=process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173/',
+    methods:['GET','POST','DELETE','PUT'],
+    allowedHeaders:[
+      "Content-Type",
+      "Authorization",
+      'Cache-Control',
+      'Expires',
+      'Pragma'
+    ],
+    credentials:true
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT,()=>console.log(`Server is running now ${PORT}`));
