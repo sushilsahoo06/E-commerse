ce6da092fbbfa726943c8bbd25f29e6e41620927
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-04 14:13:35 +0530

    new commit

--- client/src/components/common/CommonFrom.jsx
+++ client/src/components/common/CommonFrom.jsx
@@ -1,14 +1,103 @@
-import React from 'react'
+import { Label } from "@radix-ui/react-label";
+import React from "react";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@radix-ui/react-select";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
-export default function CommonFrom({fromControls}) {
+const types = {
+  INPUT: "input",
+  TEXT: "text",
+  TEXTAREA: "textarea",
+};
+export default function CommonFrom({
+  fromControls,
+  formData,
+  setformData,
+  onSubmit,
+  BUttonText,
+}) {
+  function renderInputByComponentType(getControlItem) {
+    const element = null;
+
+    const value = formData[getControlItem.name];
+    switch (getControlItem.componentType) {
+      case types.INPUT:
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+      case types.TEXT:
+        element = (
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue
+                placeholder={getControlItem.placeholder}
+              ></SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem.options && getControlItem.options.length > 0
+                ? getControlItem.options.map((optionItem) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.lebel}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+
+        break;
+      case types.TEXTAREA:
+        element = (
+          <Textarea
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.id}
+          />
+        );
+
+        break;
+
+      default:
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+        break;
+    }
+    return element;
+  }
+
   return (
-    <form>
-      <div className='flex flex-col gap-3'>
-          {
-            fromControls.map(cont)
-          }
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col gap-3">
+        {fromControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.key}>
+            <Label className="mb-1">{controlItem.Label}</Label>
+            {renderInputByComponentType(controlItem)}
+          </div>
+        ))}
       </div>
-
+      <Button type="submit" className="mt-2 w-full">
+        {BUttonText || "Submit"}
+      </Button>
     </form>
-  )
+  );
 }
