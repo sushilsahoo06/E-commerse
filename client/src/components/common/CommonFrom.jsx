a9fbef8660528020f5100158a227eb8e2273ad5d
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-04 12:06:03 +0530

    new commit

--- client/src/components/common/CommonFrom.jsx
+++ client/src/components/common/CommonFrom.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function CommonFrom() {
+export default function CommonFrom({fromControls}) {
   return (
     <form>
-      
+      <div className='flex flex-col gap-3'>
+          {
+            fromControls.map(cont)
+          }
+      </div>
+
     </form>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,3 +10,15 @@ export default defineConfig({
   ],
 })
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src')
+//     }
+//   }
+// });
