07ef7d26915201cafed31afb827bbfb9d00d2e0e
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-07 23:52:25 +0530

    commit

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Button } from '../ui/button'
-import { SquareMenu } from 'lucide-react'
+import { LogOut, SquareMenu } from 'lucide-react'
+
 
 export default function Header() {
   return (
     <header className='flex items-center justify-between px-4 py-3 bg-background'>
-      <Button className={'lg:hidden sm:block'}>
-        <SquareMenu />
+      <Button className='lg:hidden sm:block'>
+        <SquareMenu/>
+        <span className='sr-only'>Toggle menu</span>
       </Button>
+      <div>
+        <Button className='flex flex-1 justify-end'>
+          <LogOut/>
+          Logout
+        </Button>
+      </div>
+
     </header>
+
   )
 }
+
+
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // 👈 add this line
+      "@": path.resolve(__dirname, "src"), // 👈 add this line
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 5173, // or whatever port you're using
+  },
 });
-
-
