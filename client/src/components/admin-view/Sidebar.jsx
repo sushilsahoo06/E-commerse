693c69b434d94c7685aa68fb34fd54f095afd4b3
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-10 00:42:16 +0530

    commit

--- client/src/components/admin-view/Sidebar.jsx
+++ client/src/components/admin-view/Sidebar.jsx
@@ -28,14 +28,19 @@ const adminSidebarMenuItems = [
   },
 ];
 
-function MenuItems() {
+function MenuItems({setOpen}) {
+  console.log(setOpen)
   const navigate = useNavigate();
   return (
     <nav className="mt-8 flex-col flex gap-2">
       {adminSidebarMenuItems.map((menuItem) => (
         <div
           key={menuItem.id}
-          onClick={() => navigate(menuItem.path)}
+          onClick={() => {
+            navigate(menuItem.path);
+
+            setOpen ? setOpen(false) : null
+          }}
           className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground"
         >
           {menuItem.icon}
@@ -54,12 +59,12 @@ export default function Sidebar({open, setOpen}) {
         <SheetContent side="right" className="w-64">
           <div className="flex flex-col h-full">
             <SheetHeader className="border-b">
-              <SheetTitle>
+              <SheetTitle className='flex gap-2 mt-4 mb-3'>
                 <ChartNoAxesCombined size={30} />
-                Admin Panel
+                <span className="text-xl font-bold">Admin pannel</span>
               </SheetTitle>
             </SheetHeader>
-            <MenuItems />
+            <MenuItems setOpen={setOpen}/>
           </div>
         </SheetContent>
       </Sheet>
@@ -72,7 +77,7 @@ export default function Sidebar({open, setOpen}) {
           <ChartNoAxesCombined size={30} />
           <h1 className="text-2xl font-extrabold">Admin pannel</h1>
         </div>
-        <MenuItems />
+        <MenuItems/>
       </aside>
     </Fragment>
   );
