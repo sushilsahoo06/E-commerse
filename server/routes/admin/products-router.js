dd0da7cf3d530bfda1ad9d42b6512eb4ab411d1d
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-13 23:40:49 +0530

    commit

--- server/routes/admin/products-router.js
+++ server/routes/admin/products-router.js
@@ -1,10 +1,21 @@
-const express=require('express');
-const {handleImageUpload}=require('../../controllers/admin/productController');
-const {upload}=require('../../helper/cloudinary')//import multer
+const express = require("express");
+const {
+  handleImageUpload,
+  addProduct,
+  fetchAllProduct,
+  editProduct,
+  deleteProduct,
+} = require("../../controllers/admin/productController");
+const { upload } = require("../../helper/cloudinary"); //import multer
 
-const router=express.Router();
+const router = express.Router();
 
-router.post('/upload-image',upload.single('my_file'),handleImageUpload);
-console.log(upload)
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+console.log(upload);
 
-module.exports=router;
+router.post('/add',addProduct);
+router.put('/edit/:id',editProduct);
+router.delete('/delete/:id',deleteProduct);
+router.get('/get',fetchAllProduct)
+
+module.exports = router;
