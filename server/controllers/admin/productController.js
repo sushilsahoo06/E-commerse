e30e1cb3dfd640beb035b908b65b49cfcd15616a
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-13 18:18:35 +0530

    commit

--- server/controllers/admin/productController.js
+++ server/controllers/admin/productController.js
@@ -1,25 +1,76 @@
 const { imageUploadUtill } = require("../../helper/cloudinary");
 
-async function handleImageUpload(req,res) {
-  try{
-    const bs4 = Buffer.from(req.file.buffer).toString('base64'); 
+async function handleImageUpload(req, res) {
+  try {
+    const bs4 = Buffer.from(req.file.buffer).toString("base64");
     const url = `data:${req.file.mimetype};base64,${bs4}`;
 
-    const result=await imageUploadUtill(url)
+    const result = await imageUploadUtill(url);
 
     res.json({
-      success:true,
-      result
-    })
-
-  }
-  catch(error){
-    console.log(error)
+      success: true,
+      result,
+    });
+  } catch (error) {
+    console.log(error);
     res.json({
-      success:false,
-      message:"error Occured"
-    })
+      success: false,
+      message: "error Occured",
+    });
   }
 }
+//add anew product
+const addProduct = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
 
-module.exports={handleImageUpload};
+//fetch all product
+const fetchProduct = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+//edit product
+const editProduct = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+//delete product
+const deleteProduct = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  fetchProduct,
+  deleteProduct,
+  editProduct,
+};
