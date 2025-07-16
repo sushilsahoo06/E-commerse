e08c4fdd174ce3db2492f726f6ed884cbda2e996
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-16 22:47:47 +0530

    commit

--- client/src/components/shopping-view/Header.jsx
+++ client/src/components/shopping-view/Header.jsx
@@ -1,9 +1,14 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHouseUser } from '@fortawesome/free-solid-svg-icons'
-
+import { Sheet,SheetContent,SheetTrigger } from '../ui/sheet'
+import { Button } from '../ui/button'
+import { Menu } from 'lucide-react'
+// import Variants from '../../motion/Variants'
 export default function Header() {
+  
   return (
     <header className='sticky top-0 w-full border-b bg-background z-0'>
       <div className='flex h-16 items-center justify-between px-4 md:px-6'>
@@ -11,7 +16,20 @@ export default function Header() {
           <FontAwesomeIcon icon={faHouseUser} className='h-7 w-7'/>
           <span className='font-bold'>Ecommerse</span>
         </Link>
-        
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className='lg:hidden' variant='outline' size='icon'>
+              <Menu className='h-6 w-6'/>
+              <span className='sr-only'>Toggle Header Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side='left' className='w-full max-w-xs'>
+
+          </SheetContent>
+        </Sheet>
+        <div className='hidden lg:block'>
+
+        </div>
       </div>
     </header>
   )
