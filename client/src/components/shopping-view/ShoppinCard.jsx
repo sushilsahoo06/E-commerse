e090faa4357f17c2429d5b5535ebb3ed923f38b0
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-18 23:37:33 +0530

    commit

--- client/src/components/shopping-view/ShoppinCard.jsx
+++ client/src/components/shopping-view/ShoppinCard.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
+import { brandOptionsMap, categoryOptionsMap } from "@/config";
 
 export default function ShoppinCard({ product }) {
-  console.log(product, "product card");
+ 
   return (
     <Card className="w-full max-w-sm mx-auto">
       <div>
@@ -14,29 +15,42 @@ export default function ShoppinCard({ product }) {
             alt={product?.title}
             className="w-full h-[300px] rounded-t-lg object-cover"
           />
+          {product?.salePrice > 0 ? <Badge className='absolute top-2 left-2 bg-red-500 hover:bg-red-600 '>Sale</Badge> : null}
         </div>
-        {product?.salePrice > 0 ? <Badge>Sale</Badge> : null}
+        
+        
       </div>
 
       <CardContent className="p-4">
-        <h2 className="font-bold text-xl mb-2">{product?.title}</h2>
-        <div>
+        <h2 className=" font-bold text-xl mb-2">{product?.title}</h2>
+        <div className="flex items-center justify-between mb-2">
+          <span className="text-[16px] text-muted-foreground">
+            {categoryOptionsMap[product?.category]}
+          </span>
+          <span className="text-[16px] text-muted-foreground">
+            {brandOptionsMap[product?.brand]}
+          </span>
+
+
+        </div>
+        <div className="flex justify-between items-center mb-2"> 
+
           <span
             className={`${
               product?.salePrice > 0 ? "line-through" : ""
             } text-lg font-semibold text-primary`}
           >
-            {product?.price}
+            ${product?.price}
           </span>
           {product?.salePrice > 0 ? (
             <span className="text-lg font-semibold text-primary">
-              {product?.brand}
+              ${product?.salePrice}
             </span>
           ) : null}
         </div>
       </CardContent>
-      <CardFooter className="w-full">
-        <Button>Add to cart</Button>
+      <CardFooter >
+        <Button className='w-full'>Add to cart</Button>
       </CardFooter>
     </Card>
   );
--- client/src/pages/shopping-view/ShoppingListing.jsx
+++ client/src/pages/shopping-view/ShoppingListing.jsx
@@ -11,16 +11,23 @@ import {
 import { sortOptions } from "@/config";
 import { fetchAllFilteredProducts } from "@/store/shop/product-slice";
 import { ArrowUpDown } from "lucide-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function ShoppingListing() {
   const dispatch = useDispatch();
   const { ProductList } = useSelector((state) => state.shopProduct);
-  console.log(ProductList, "productlist");
+  const [filters, setFilters] = useState(null);
+  const [sort, setSort] = useState(null);
+
+  function handleSort(value) {
+    console.log(value);
+    setSort(value);
+  }
   useEffect(() => {
     dispatch(fetchAllFilteredProducts());
   }, [dispatch]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6">
       <Filter />
@@ -28,7 +35,9 @@ export default function ShoppingListing() {
         <div className="p-4 border-b flex items-center justify-between">
           <h2 className="text-lg font-extrabold">All Products</h2>
           <div className="flex items-center gap-3">
-            <span className="text-muted-foreground">10 products</span>
+            <span className="text-muted-foreground">
+              {ProductList.length} products
+            </span>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
@@ -41,9 +50,12 @@ export default function ShoppingListing() {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-[200px]">
-                <DropdownMenuRadioGroup>
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
                   {sortOptions.map((sortItem) => (
-                    <DropdownMenuRadioItem key={sortItem.id}>
+                    <DropdownMenuRadioItem
+                      key={sortItem.id}
+                      value={sortItem.id}
+                    >
                       {sortItem.label}
                     </DropdownMenuRadioItem>
                   ))}
@@ -55,9 +67,7 @@ export default function ShoppingListing() {
         <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
           {ProductList && ProductList.length > 0
             ? ProductList.map((productItem) => {
-                return (
-                  <ShoppinCard  product={productItem} />
-                );
+                return <ShoppinCard product={productItem} />;
               })
             : null}
         </div>
