fabc734260340139efa16f5b2a0a9c59c789d218
sushilsahoo06 <sahoosushil456@gmail.com>
2025-07-18 12:18:50 +0530

    commit

---
+++ client/src/components/shopping-view/ShoppinCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+
+export default function ShoppinCard({ product }) {
+  return (
+    <Card className="w-full max-w-sm max-auto">
+      <div>
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="w-dull h-[300px] rounded-t-lg object-cover"
+          />
+        </div>
+        {product?.salePrise > 0 ? <Badge>Sale</Badge> : null}
+      </div>
+
+      <CardContent className="p-4">
+        <h2 className="font-bold text-xl mb-2">{product?.title}</h2>
+        <div>
+          <span
+            className={`${
+              product?.salePrise > 0 ? "line-through" : ""
+            } text-lg font-semibold text-primary`}
+          >
+            {product?.price}
+          </span>
+          {product?.salePrise > 0 ? (
+            <span className="text-lg font-semibold text-primary">
+              {product?.brand}
+            </span>
+          ) : (
+            null
+          )}
+        </div>
+      </CardContent>
+      <CardFooter className='w-full'><Button>Add to cart</Button></CardFooter>
+    </Card>
+  );
+}
